355304b9ca0add0f540f83f05bb4fc82e3cd721d
Gaetanneo <130810061+Gaetanneo@users.noreply.github.com>
2023-11-13 00:08:24 -0500

    next change

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call() {
     withSonarQubeEnv('sonar-server') {
-        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=test4 -Dsonar.projectKey=youtube '''
+        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Youtube-cicd -Dsonar.projectKey=youtube1 '''
     }
 }
